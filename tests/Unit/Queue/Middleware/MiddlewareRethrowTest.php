191e2a4a3a798ee2a8b38a3626fb43565c2bbdd2
geangontijo <geangontijo07@gmail.com>
2026-02-25 18:28:57 -0300

    test(middleware): atualizar teste para re-lançamento de exceções não-retryable com uso de factory

--- tests/Unit/Queue/Middleware/MiddlewareRethrowTest.php
+++ tests/Unit/Queue/Middleware/MiddlewareRethrowTest.php
@@ -1,25 +1,34 @@
 <?php
 
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Http;
 use MobileStock\LaravelResilience\Queue\Middleware\HttpClientRetryMiddleware;
 use MobileStock\LaravelResilience\Queue\Middleware\RetryableExceptionMiddleware;
 use MobileStock\LaravelResilience\Queue\Middleware\RetryDeadlockMiddleware;
 
 it('should re-throw exception and not release job when non-retryable exception is thrown', function (
-    object $middleware
+    object $middleware,
+    Closure $exceptionFactory
 ) {
     $job = Mockery::spy();
-    $exception = new Exception('Normal exception');
+    $exception = $exceptionFactory();
     $next = function () use ($exception) {
         throw $exception;
     };
 
     $call = fn() => $middleware->handle($job, $next);
 
-    expect($call)->toThrow(Exception::class, 'Normal exception');
+    expect($call)->toThrow($exception::class, $exception->getMessage());
     $job->shouldNotHaveReceived('release');
     $job->shouldNotHaveReceived('fail');
 })->with([
-    'HttpClientRetryMiddleware' => new HttpClientRetryMiddleware(),
-    'RetryDeadlockMiddleware' => new RetryDeadlockMiddleware(),
-    'RetryableExceptionMiddleware' => new RetryableExceptionMiddleware(),
+    'HttpClientRetryMiddleware' => [
+        new HttpClientRetryMiddleware(),
+        fn() => new RequestException(Http::fake(['*' => Http::response([], 500)])->get('http://dummy.com')),
+    ],
+    'RetryDeadlockMiddleware' => [
+        new RetryDeadlockMiddleware(),
+        fn() => new Illuminate\Database\QueryException('connection', 'sql', [], new PDOException('non-deadlock')),
+    ],
+    'RetryableExceptionMiddleware' => [new RetryableExceptionMiddleware(), fn() => new Exception('Normal exception')],
 ]);
