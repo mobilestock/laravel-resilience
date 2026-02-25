d870b7065e10e0615acfd88e695bfef21c3dfe10
geangontijo <geangontijo07@gmail.com>
2026-02-25 19:13:53 -0300

    refactor(provider): reorganizar lógica de publicação e extensão de comandos no ResilienceServiceProvider

--- src/Providers/ResilienceServiceProvider.php
+++ src/Providers/ResilienceServiceProvider.php
@@ -12,23 +12,24 @@ class ResilienceServiceProvider extends ServiceProvider
 {
     public function boot(Dispatcher $bus, Repository $config): void
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes(
-                [
-                    __DIR__ . '/../../config/resilience.php' => $this->app->configPath('resilience.php'),
-                ],
-                'resilience-config'
-            );
-
-            foreach ([LaravelWorkCommand::class, LaravelListenCommand::class] as $command) {
-                $this->app->extend($command, function ($command) {
-                    $command->getDefinition()->getOption('tries')->setDefault(0);
-
-                    return $command;
-                });
-            }
+        if (!$this->app->runningInConsole()) {
+            return;
         }
 
+        $this->publishes(
+            [
+                __DIR__ . '/../../config/resilience.php' => $this->app->configPath('resilience.php'),
+            ],
+            'resilience-config'
+        );
+
+        foreach ([LaravelWorkCommand::class, LaravelListenCommand::class] as $command) {
+            $this->app->extend($command, function ($command) {
+                $command->getDefinition()->getOption('tries')->setDefault(0);
+
+                return $command;
+            });
+        }
         $bus->pipeThrough($config->get('resilience.middlewares'));
     }
 
